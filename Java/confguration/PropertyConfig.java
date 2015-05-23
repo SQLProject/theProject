75fcba616c891dd5a1ed392282c607268d9eae77
michalidisis <michi.idisis@gmail.com>
2015-05-23 14:02:57 +0300

    test

--- Java/confguration/PropertyConfig.java
+++ Java/confguration/PropertyConfig.java
@@ -2,7 +2,7 @@ package Confguration;
 
 import java.io.IOException;
 import java.util.Properties;
-//Configuration Class HELLO
+//Configuration Class HELLO HI!!!!
 public class PropertyConfig {
 
     private Properties configFile = new Properties();
