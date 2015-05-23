81affced3f9cd3f12bb5beed993147456adb019f
michalidisis <michi.idisis@gmail.com>
2015-05-23 17:49:31 +0300

    updated parser tester, yagoFacts parser and more

--- Java/parser/Parser_Tester.java
+++ Java/parser/Parser_Tester.java
@@ -1,10 +1,29 @@
 package parser;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import entities.Country;
+
 public class Parser_Tester {
 
 	public static void main(String[] args) {
 		TransitiveType_Parser transitive_pars=new TransitiveType_Parser();
-		transitive_pars.parse_transitive_type();
+		Set<String> nameSet=(transitive_pars.getCountriesMap()).keySet();
+		for(int i=0; i<100; i++){
+			System.out.println(nameSet);
+		}
+		nameSet=transitive_pars.getPlayersMap().keySet();
+		for(int i=0; i<100; i++){
+			System.out.println(nameSet);
+		}
+		nameSet=transitive_pars.getCoachesMap().keySet();
+		for(int i=0; i<100; i++){
+			System.out.println(nameSet);
+		}
+		//Facts_Parser facts_pars= new Facts_Parser(transitive_pars.getCountriesMap(),
+			//	transitive_pars.getCitiesMap(),	transitive_pars.getPlayersMap(), transitive_pars.getCoachesMap());
 	}
 
 }
