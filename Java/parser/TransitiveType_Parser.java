1de59db86217f108c8955f2a89f7be5b91be6afb
Kobi Stav <stiv1qaz1@gmail.com>
2015-05-24 15:25:43 +0300

    TranFile

--- Java/parser/TransitiveType_Parser.java
+++ Java/parser/TransitiveType_Parser.java
@@ -6,29 +6,22 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
-import entities.City;
-import entities.Coach;
-import entities.Country;
-import entities.Event;
-import entities.SportField;
-import entities.Player;
-import entities.Team;
-
-
+import entities.*;
+;
 
 
 public class TransitiveType_Parser extends abstract_parser{
-		
+	public static int DEBUG=1;
 	public TransitiveType_Parser(){
 		this.countriesMap= new HashMap<String,Country>();
 		this.citiesMap= new HashMap<String,City>();
 		this.playersMap= new HashMap<String,Player>();
 		this.coachesMap=new HashMap<String,Coach>();
-		this.eventsMap= new HashMap<String,Event>();
 		this.teamsMap = new HashMap<String,Team>();
+		this.locationMap=new HashMap<String, Location>();
 	}
 	
-	protected void parse_transitive_type(){
+	public void parse_transitive_type(){
 		String yagoTransitiveType_file_path = config.get_yago_transitive_types_path();
 		
 		/*try to open the yagoTansetiveTypes file*/
@@ -46,26 +39,31 @@ public class TransitiveType_Parser extends abstract_parser{
 			br = new BufferedReader(fr);
 
 			while((line= br.readLine())!= null){
-				
+				if (DEBUG==1){
+					if (countriesMap.size()>3){
+						break;}
+				}
 				/* find all the countries with the proper tag */
 				if(line.contains("<wikicat_Countries>")){
 					Country newCountry=getCountryFromLine(line);
 					countriesMap.put(newCountry.getName(), newCountry);
+					locationMap.put(newCountry.getName(), newCountry);
 				}
 				
 				/* find all the cities with the proper tag */
 				if(line.contains("<wikicat_Cities>") || line.contains("<wordnet_city_")){
 					City newCity=getCityFromLine(line);
 					citiesMap.put(newCity.getName(),newCity);
+					locationMap.put(newCity.getName(),newCity);
 				}
 				
-				/* find all basketball teams */
+					/* find all basketball teams */
 				if(line.contains("<wordnet_basketball_team")){
 					Team newTeam = getTeamFromLine(line);
 					newTeam.setSportField(SportField.BASKETBALL);
 					teamsMap.put(newTeam.getTeamName(), newTeam);
 				}
-				/* find all football teams */
+					/* find all football teams */
 				if(line.contains("<wordnet_football_team")){
 					Team newTeam = getTeamFromLine(line);
 					newTeam.setSportField(SportField.FOOTBALL);
@@ -94,15 +92,6 @@ public class TransitiveType_Parser extends abstract_parser{
 					if (line.contains("_tennis_"))
 						coach.setSportField(SportField.TENNIS);
 					coachesMap.put(coach.getName(), coach);
-				}
-				
-				/* find all the events with the proper tag */
-				if(line.contains("<wikicat") && line.contains("competitions")) {
-					Event newEvent=getEventFromLine(line);
-					if (newEvent != null) {
-						eventsMap.put(newEvent.getName(),newEvent);
-					}
-					
 				}
 				
 			}
@@ -157,29 +146,6 @@ public class TransitiveType_Parser extends abstract_parser{
 		line = line.substring(line.indexOf('>',0)+1);
 		String team_name = getTag(line);
 		return new Team(yagoID,team_name,0);
-	}
-	
-	protected Event getEventFromLine(String line){
-		String yagoID=getTag(line);
-		line=line.substring(line.indexOf('>',0)+1);
-		String event_name=getTag(line);
-		SportField typeOfSport=null;
-		
-		if(line.contains("football") || line.contains("soccer")) {
-			typeOfSport=SportField.FOOTBALL;
-		}
-		else if(line.contains("basketball")) {
-			typeOfSport=SportField.BASKETBALL;
-		}
-		else {
-			return null;
-		}
-		
-		//DEBUG
-		//System.out.println("The event yagoID is:"+yagoID+"\t The event name is:"+event_name+"\t The sport field is:"+typeOfSport.getKind());
-		//DEBUG
-		
-		return new Event(yagoID,event_name,0,null,typeOfSport);	//TODO:ID and HappendIn
 	}
 
 }
