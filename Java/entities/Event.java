7250559724400abbc27ec5cc1cb443813964a2e1
roynahmias <roynahmias9@gmail.com>
2015-05-24 10:47:48 +0300

    Update Event.java
    Roy - updated the Event code in the arabic way ;)

--- Java/entities/Event.java
+++ Java/entities/Event.java
@@ -6,5 +6,22 @@ public class Event {
 	String name;
 	int id;
 	Location happendIn;
+	SportField typeOfSport;
+	
+	public Event (String yagoID, String event_name,int id, Location happendIn, SportField typeOfSport){
+		this.yagoID=yagoID;
+		this.name=event_name;
+		this.id=id;
+		this.happendIn = happendIn;
+		this.typeOfSport = typeOfSport;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public String getYagoID(){
+		return this.yagoID;
+	}
 
 }
